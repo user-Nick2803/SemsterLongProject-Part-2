cea4966e01a111a34e9589c9d1eb329961106a95
user-Nick2803 <nmbbrown43@gmail.com>
2024-10-27 19:45:57 -0400

    Add files via upload
    This is the finished version of the first release. I figured out how to get it to compile

--- myos/include/console.h
+++ myos/include/console.h
@@ -3,7 +3,7 @@
 
 static const int VGA_WIDTH = 80;
 static const int VGA_HEIGHT = 25;
-static const int VGA_BYTES_PER_CHARACTER=2;
+static const int VGA_BYTES_PER_CHARACTER = 2;
 
 void clear_terminal();
 void print_character(char c);
@@ -11,3 +11,4 @@ void print_string(char* str);
 void print_line(char* str);
 
 #endif
+
--- myos/kernel.c
+++ myos/kernel.c
@@ -3,16 +3,7 @@
 #define VGA_HEIGHT 25
 
 
-void clear_terminal(){
 
-char* vga = VGA_MEMORY;
-  
-  for(int i = 0; i < VGA_WIDTH * VGA_HEIGHT * 2; i += 2){
-  vga[i] = '\0'; //null character for blank space
-  vga[i + 1] = 0x07;
-  }
-  
-}
 
 void main() {
         
@@ -24,8 +15,7 @@ void main() {
 	for (int i=0; str[i] != '\0'; i++) {
 		VGA_BUFFER[i*2] = str[i];
 	}
-        
-        clear_terminal();
+      
         print_string("HELLO");
         print_line("WORLD");
         print_string("TODAY");
